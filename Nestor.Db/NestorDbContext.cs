84583649bf02247f69d581e6b335f26d522d4a9b
Serhii Maksymov <maksimovsergey08@gmail.com>
2025-07-19 08:30:44 +0300

    add constructor overloads to NestorDbContext for improved configuration support

--- Nestor.Db/NestorDbContext.cs
+++ Nestor.Db/NestorDbContext.cs
@@ -2,8 +2,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Nestor.Db;
 
-public class NestorDbContext<TConfiguration> : DbContext where TConfiguration : IEntityTypeConfiguration<EventEntity>, new()
+public class NestorDbContext<TConfiguration> : DbContext
+    where TConfiguration : IEntityTypeConfiguration<EventEntity>, new()
 {
+    public NestorDbContext()
+    {
+    }
+
+    public NestorDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new TConfiguration());
