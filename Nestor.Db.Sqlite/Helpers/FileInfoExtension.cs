bcf8bc73f5cdd89cc31b8dc3d74ab36cd5b3b9f9
gubard <maksimovsergey08@gmail.com>
2025-12-07 16:55:35 +0200

    Add FileInfoExtension for SQLite DB initialization and update project references

---
+++ Nestor.Db.Sqlite/Helpers/FileInfoExtension.cs
@@ -0,0 +1,32 @@
+﻿using Gaia.Helpers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Nestor.Db.Sqlite.Helpers;
+
+public static class FileInfoExtension
+{
+    public static SqliteNestorDbContext InitDbContext(this FileInfo file)
+    {
+        var context = new SqliteNestorDbContext(new DbContextOptionsBuilder<SqliteNestorDbContext>().UseSqlite($"Data Source={file}", x => x.MigrationsAssembly(typeof(SqliteNestorDbContext).Assembly)).Options);
+        var migrationFile = file.FileInSameDir($"{file.GetFileNameWithoutExtension()}.migration");
+        var lastMigration = context.Database.GetMigrations().Last();
+
+        if (!file.Exists)
+        {
+            if (file.Directory is { Exists: false })
+            {
+                file.Directory.Create();
+            }
+
+            context.Database.Migrate();
+            migrationFile.WriteAllText(lastMigration);
+        }
+        else if (lastMigration != migrationFile.ReadAllText())
+        {
+            context.Database.Migrate();
+            migrationFile.WriteAllText(lastMigration);
+        }
+
+        return context;
+    }
+}
