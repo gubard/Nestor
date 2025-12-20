1537a32eff88d4446cf9267b312127e6a6047ad3
gubard <maksimovsergey08@gmail.com>
2025-12-20 10:19:47 +0200

    Refactor `InitDbContext` to improve readability by splitting `DbContextOptionsBuilder` configuration into a separate variable.

--- Nestor.Db.Sqlite/Helpers/FileInfoExtension.cs
+++ Nestor.Db.Sqlite/Helpers/FileInfoExtension.cs
@@ -7,15 +7,14 @@ public static class FileInfoExtension
 {
     public static SqliteNestorDbContext InitDbContext(this FileInfo file)
     {
-        var context = new SqliteNestorDbContext(
-            new DbContextOptionsBuilder<SqliteNestorDbContext>()
-                .UseSqlite(
-                    $"Data Source={file}",
-                    x => x.MigrationsAssembly(typeof(SqliteNestorDbContext).Assembly)
-                )
-                .Options
-        );
+        var options = new DbContextOptionsBuilder<SqliteNestorDbContext>()
+            .UseSqlite(
+                $"Data Source={file}",
+                x => x.MigrationsAssembly(typeof(SqliteNestorDbContext).Assembly)
+            )
+            .Options;
 
+        var context = new SqliteNestorDbContext(options);
         var migrationFile = file.FileInSameDir($"{file.GetFileNameWithoutExtension()}.migration");
         var lastMigration = context.Database.GetMigrations().Last();
 
