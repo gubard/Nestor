84ab2facd40b6e217b892d1470fd26e473262b2e
gubard <maksimovsergey08@gmail.com>
2026-01-03 22:53:54 +0200

    Refactored `InitDbContext` to delegate migration handling to `IMigrator`.

--- Nestor.Db.Sqlite/Helpers/FileInfoExtension.cs
+++ Nestor.Db.Sqlite/Helpers/FileInfoExtension.cs
@@ -1,35 +1,18 @@
-﻿using Gaia.Helpers;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Nestor.Db.Services;
 
 namespace Nestor.Db.Sqlite.Helpers;
 
 public static class FileInfoExtension
 {
-    public static SqliteNestorDbContext InitDbContext(this FileInfo file)
+    public static SqliteNestorDbContext InitDbContext(this FileInfo file, IMigrator migrator)
     {
         var options = new DbContextOptionsBuilder<SqliteNestorDbContext>()
             .UseSqlite($"Data Source={file}")
             .Options;
 
         var context = new SqliteNestorDbContext(options);
-        var migrationFile = file.FileInSameDir($"{file.GetFileNameWithoutExtension()}.migration");
-        var lastMigration = "20251217141859_AddIsLast";
-
-        if (!file.Exists)
-        {
-            if (file.Directory is { Exists: false })
-            {
-                file.Directory.Create();
-            }
-
-            context.Database.Migrate();
-            migrationFile.WriteAllText(lastMigration);
-        }
-        else if (lastMigration != migrationFile.ReadAllText())
-        {
-            context.Database.Migrate();
-            migrationFile.WriteAllText(lastMigration);
-        }
+        migrator.Migrate(context);
 
         return context;
     }
