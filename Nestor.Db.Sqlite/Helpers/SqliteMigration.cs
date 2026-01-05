494d8b3a9649c9aab819a646865f6f31562de3d3
gubard <maksimovsergey08@gmail.com>
2026-01-05 19:18:37 +0200

    Update SQL schema to include stricter constraints and checks

--- Nestor.Db.Sqlite/Helpers/SqliteMigration.cs
+++ Nestor.Db.Sqlite/Helpers/SqliteMigration.cs
@@ -13,50 +13,45 @@ public static class SqliteMigration
             {
                 202601031118,
                 @"
-create table MigrationEntity
-(
-    Id                        INTEGER           not null
-        constraint PK_MigrationEntity,
-    Sql         TEXT
-)
+CREATE TABLE IF NOT EXISTS MigrationEntity (
+    Id INTEGER PRIMARY KEY NOT NULL,
+    Sql TEXT NOT NULL
+);
 "
             },
             {
                 202601031119,
                 @"
-create table EventEntity
-(
-    Id                        INTEGER           not null
-        constraint PK_EventEntity
-            primary key autoincrement,
-    EntityId                  TEXT              not null,
-    EntityType                TEXT              not null,
-    EntityProperty            TEXT              not null,
-    UserId                    TEXT              not null,
-    CreatedAt                 TEXT              not null,
-    EntityBooleanValue        INTEGER,
-    EntityByteValue           INTEGER,
-    EntityUInt16Value         INTEGER,
-    EntityUInt32Value         INTEGER,
-    EntityUInt64Value         INTEGER,
-    EntitySByteValue          INTEGER,
-    EntityInt16Value          INTEGER,
-    EntityInt32Value          INTEGER,
-    EntityInt64Value          INTEGER,
-    EntitySingleValue         REAL,
-    EntityDoubleValue         REAL,
-    EntityDecimalValue        TEXT,
-    EntityCharValue           TEXT,
-    EntityByteArrayValue      BLOB,
-    EntityStringValue         TEXT,
-    EntityGuidValue           TEXT,
-    EntityDateTimeValue       TEXT,
+CREATE TABLE IF NOT EXISTS EventEntity (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    EntityId TEXT NOT NULL,
+    EntityType TEXT NOT NULL CHECK(length(EntityType) <= 255),
+    EntityProperty TEXT NOT NULL CHECK(length(EntityProperty) <= 255),
+    UserId TEXT NOT NULL CHECK(length(UserId) <= 255),
+    CreatedAt TEXT NOT NULL,
+    EntityBooleanValue INTEGER CHECK (EntityBooleanValue IN (0, 1)),
+    EntityByteValue INTEGER,
+    EntityUInt16Value INTEGER,
+    EntityUInt32Value INTEGER,
+    EntityUInt64Value INTEGER,
+    EntitySByteValue INTEGER,
+    EntityInt16Value INTEGER,
+    EntityInt32Value INTEGER,
+    EntityInt64Value INTEGER,
+    EntitySingleValue REAL,
+    EntityDoubleValue REAL,
+    EntityDecimalValue TEXT,
+    EntityCharValue TEXT CHECK(length(EntityCharValue) <= 1),
+    EntityByteArrayValue BLOB,
+    EntityStringValue TEXT,
+    EntityGuidValue TEXT,
+    EntityDateTimeValue TEXT,
     EntityDateTimeOffsetValue TEXT,
-    EntityDateOnlyValue       TEXT,
-    EntityTimeOnlyValue       TEXT,
-    EntityTimeSpanValue       TEXT,
-    IsLast                    INTEGER default 0 not null
-)
+    EntityDateOnlyValue TEXT,
+    EntityTimeOnlyValue TEXT,
+    EntityTimeSpanValue TEXT,
+    IsLast INTEGER NOT NULL CHECK (IsLast IN (0, 1))
+);
 "
             },
         }.ToFrozenDictionary();
