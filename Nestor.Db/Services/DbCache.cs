5fa4a4c7b00d50ad534c90d4c8d21de0268de899
gubard <maksimovsergey08@gmail.com>
2026-01-10 20:47:46 +0200

    Add MelnikovViewModelFactory and DbCache implementations
    Introduced the `MelnikovViewModelFactory` to create `SignInViewModel` instances and registered it as a transient service in `MelnikovServiceProvider`. Additionally, created the `DbCache` interface in the Nestor.Db.Services namespace to handle data cache updates. These changes enhance modularity and simplify dependency management.

---
+++ Nestor.Db/Services/DbCache.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Nestor.Db.Services;
+
+public interface IDbCache<in TSource>
+{
+    ConfiguredValueTaskAwaitable UpdateAsync(TSource source, CancellationToken ct);
+    void Update(TSource source);
+}
