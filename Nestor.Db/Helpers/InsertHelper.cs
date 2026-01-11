1cc7e27692cacef9774f597b2941528ea26d7c1b
gubard <maksimovsergey08@gmail.com>
2026-01-11 15:29:32 +0200

    Add InsertHelper for managing default insert queries

---
+++ Nestor.Db/Helpers/InsertHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Nestor.Db.Models;
+
+namespace Nestor.Db.Helpers;
+
+public static class InsertHelper
+{
+    private static readonly Dictionary<string, Func<Guid, SqlQuery>> Factories = new();
+
+    public static SqlQuery CreateDefaultInsert(string entityName, Guid id)
+    {
+        return Factories[entityName].Invoke(id);
+    }
+
+    public static void AddDefaultInsert(string entityName, Func<Guid, SqlQuery> func)
+    {
+        Factories.Add(entityName, func);
+    }
+}
