807dd8e418ab1cbee394c5c8be58c67b57c0197a
gubard <maksimovsergey08@gmail.com>
2026-03-28 20:21:34 +0200

    Add `ValueDatabaseFactory` implementation to simplify database creation logic

--- Nestor.Db.LiteDb/Services/FileDatabaseFactory.cs
+++ Nestor.Db.LiteDb/Services/FileDatabaseFactory.cs
@@ -1,8 +1,24 @@
 using System.Runtime.CompilerServices;
+using Gaia.Helpers;
 
 namespace Nestor.Db.LiteDb.Services;
 
 public interface IDatabaseFactory
 {
     ConfiguredValueTaskAwaitable<IDatabase> CreateAsync(CancellationToken ct);
+}
+
+public sealed class ValueDatabaseFactory : IDatabaseFactory
+{
+    public ValueDatabaseFactory(IDatabase database)
+    {
+        _database = database;
+    }
+
+    public ConfiguredValueTaskAwaitable<IDatabase> CreateAsync(CancellationToken ct)
+    {
+        return TaskHelper.FromResult(_database);
+    }
+
+    private readonly IDatabase _database;
 }
